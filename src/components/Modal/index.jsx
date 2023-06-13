e3986c822d0a48dda6f8567f2e0f1a33a1e62640
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-06-13 19:31:15 -0300

    Finished Tasklist and Modal components

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
 import './styles.scss';
 
-export const Modal = () => {
+export const Modal = ({ isOpen, onClose, options, task }) => {
+    const [isModalOpen, setIsModalOpen] = useState(isOpen);
+    useEffect(() => {
+        setIsModalOpen(isOpen);
+    }, [isOpen])
+
+    const handleClose = () => {
+        setIsModalOpen(false);
+        onClose();
+    }
+
     return (
-        <div>Modal</div>
+        isModalOpen && (
+            <div className="modal-overlay">
+                <div className="modal-wrapper">
+                    <h3 className="modal-wrapper__title">
+                        {
+                            !options ? "Deseja excluir este item?" : "Deseja editar este item?"
+                        }
+                    </h3>
+                    <p className="modal-wrapper__task">{task.description}</p>
+                    <div className="modal-wrapper__btns">
+                        <button onClick={handleClose}>Não</button>
+                        <button>Sim</button>
+                    </div>
+                </div>
+            </div>
+        )
     )
 }
--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,9 +1,88 @@
+import { useState } from 'react';
 import './styles.scss';
-
-import React from 'react'
+import { CheckSquare, PencilSimple, Plus, Square, TrashSimple } from 'phosphor-react';
+import { Modal } from '../Modal';
 
 export const TaskList = ({ data }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [options, setOptions] = useState(null);
+    const [taskData, setTaskData] = useState(null);
+
+    const handleOpenModal = () => {
+        setIsOpen(true);
+    }
+
+    const handleCloseModal = () => {
+        setIsOpen(false);
+    }
+
+    const handleRemove = (item) => {
+        setOptions(false);
+        handleOpenModal();
+        setTaskData(item);
+    }
+
+    const handleEdit = (item) => {
+        setOptions(true);
+        handleOpenModal();
+        setTaskData(item);
+    }
+
+
     return (
-        <div>Task List</div>
+        <div className="tasklist">
+            <h2 className="tasklist-title" >
+                Otimize seu tempo e se organize com o nosso Planejador Diário.
+            </h2>
+            <div className="tasklist-table">
+                <Modal isOpen={isOpen} onClose={handleCloseModal} task={taskData} options={options} />
+                <table>
+                    <thead>
+                        <tr className="tasklist-table__header">
+                            <th>Tarefa</th>
+                            <th>Status</th>
+                            <th>Opções</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((item) => {
+                                return (
+                                    <tr key={item.id}>
+                                        <td>{item.title}</td>
+                                        <td className="tasklist-table__status">
+                                            {
+                                                item.completed === true
+                                                    ? <CheckSquare className="icon" weight="regular" />
+                                                    : <Square className="icon" weight="regular" />
+                                            }
+                                        </td>
+                                        <td className="tasklist-table__options">
+                                            <TrashSimple
+                                                className="icon"
+                                                onClick={() => handleRemove(item)}
+                                                weight="fill"
+                                            />
+                                            <PencilSimple
+                                                className="icon"
+                                                onClick={() => handleEdit(item)}
+                                                weight="fill"
+                                            />
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        <tr>
+                            <td className="tasklist__newTask">Nova tarefa...</td>
+                            <td></td>
+                            <td className="tasklist__newTask-button">
+                                <Plus weight="bold" size={28} />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
