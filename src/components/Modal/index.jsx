52865858b5bec745b9fce85032469445a2cf0fc1
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-06-15 19:54:29 -0300

    Implementada função de editar titulo e descrição da tarefa.

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './styles.scss';
 
-export const Modal = ({ isOpen, onClose, options, selectedTask, onDelete }) => {
+export const Modal = ({ isOpen, onClose, options, selectedTask, onDelete, setTasks, allTasks }) => {
     const [isModalOpen, setIsModalOpen] = useState(isOpen);
     const [edit, setEdit] = useState(false);
     const [editTitle, setEditTitle] = useState('');
@@ -22,7 +22,6 @@ export const Modal = ({ isOpen, onClose, options, selectedTask, onDelete }) => {
     }
 
     const handleEdit = () => {
-        console.log(edit)
         setEdit(true);
     }
 
@@ -36,6 +35,20 @@ export const Modal = ({ isOpen, onClose, options, selectedTask, onDelete }) => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        console.log(allTasks, selectedTask)
+        const updatedTasks = allTasks.map(task => {
+            if (task.id === selectedTask.id) {
+                return {
+                    ...task,
+                    title: editTitle,
+                    description: editDescription
+                };
+            }
+            return task;
+        })
+        setTasks(updatedTasks);
+        onClose();
+        setEdit(false);
     }
 
     return (
@@ -76,20 +89,22 @@ export const Modal = ({ isOpen, onClose, options, selectedTask, onDelete }) => {
                                 edit ?
                                     <>
                                         <form onSubmit={handleSubmit} className="modal-wrapper__form">
-                                            <input
-                                                onChange={handleTitleChange}
-                                                placeholder="Editar título"
-                                                value={editTitle}
-                                                type="text"
-                                                className="modal-wrapper__form-input"
-                                            />
-                                            <input
-                                                placeholder="Editar tarefa"
-                                                onChange={handleDescriptionChange}
-                                                value={editDescription}
-                                                type="text"
-                                                className="modal-wrapper__form-input"
-                                            />
+                                            <div className="modal-wrapper__form-inputs">
+                                                <input
+                                                    onChange={handleTitleChange}
+                                                    placeholder={selectedTask.title}
+                                                    value={editTitle}
+                                                    type="text"
+                                                    className="modal-wrapper__form-input"
+                                                />
+                                                <input
+                                                    placeholder={selectedTask.description}
+                                                    onChange={handleDescriptionChange}
+                                                    value={editDescription}
+                                                    type="text"
+                                                    className="modal-wrapper__form-input"
+                                                />
+                                            </div>
                                             <div className="modal-wrapper__btns">
                                                 <button type="button" onClick={handleClose}>Voltar</button>
                                                 <button type="submit">Salvar</button>
--- src/mock/DATA_MOCK.js
+++ src/mock/DATA_MOCK.js
@@ -2,5 +2,5 @@ export const db = [
     { "id": 1, "title": "Exercicios", "description": "Ir para academia fazer exercicios", "completed": true },
     { "id": 2, "title": "Limpar o carro", "description": "Limpar o carro inteiro, de dentro pra fora", "completed": false },
     { "id": 3, "title": "Banho e tosa", "description": "Levar o cachorro ao pet shop", "completed": false },
-    { "id": 4, "title": "Limpar quarto", "description": "Limpar toda bagunça que está dentro do quarto", "completed": true }
+    { "id": 4, "title": "Limpar quarto", "description": "Limpar toda bagunça que está dentro do quarto e organizar o armário", "completed": true }
 ]
